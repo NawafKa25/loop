e86fa03715492d99aade62aac27bddebdf8bfc20
Nawaf <mohammdnawaf1999@gmail.com>
2023-09-15 20:23:46 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,15 @@
+function multiplication(t,s){
+    var mul=t*s;
+    console.log('the result is '+mul)
+}
+function substraction(t,s){
+    var sub=t-s;
+    console.log('the result is '+ sub)
+}
+function division(t,s){
+    var div=t/s;
+    console.log('the result is '+div)
+}
+multiplication(10,5);
+substraction(10,5);
+division(4,2);
